aef3bf814baf134159c418fdbab14da3fa3992f2
festuskimesh <147733480+festuskimesh@users.noreply.github.com>
2025-04-02 12:36:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const global = {
-    owner: [6142885267], // ganti jadi id mu
-    botToken: "7725260979:AAF9pls5ef2mKp0eZu-djXeFnnwC5XCRyL8", //isi make token bot mu
+    owner: [6652803119], // ganti jadi id mu
+    botToken: "7355754839:AAERTWARIwdGG666Tiq3WjrZs_J_klfGpqw", //isi make token bot mu
 }
 
 module.exports = global;
